0e301dc5f5db601714e68384e9f31c1a18e7d5bd
russell <rmartin16@gmail.com>
2020-06-06 21:28:26 -0400

    add more tests and get coverity working

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,6 +44,11 @@ def torrent_hash():
 
 
 @pytest.fixture(scope='session')
+def torrent(client, torrent_hash):
+    return client.torrents_info(torrent_hashes=torrent_hash)[0]
+
+
+@pytest.fixture(scope='session')
 def app_version():
     return qbt_version
 
@@ -51,3 +56,8 @@ def app_version():
 @pytest.fixture(scope='session')
 def api_version():
     return api_version_map[qbt_version]
+
+
+def pytest_sessionfinish(session, exitstatus):
+    client = Client()
+    client.torrents_delete(delete_files=True, torrent_hashes='c6df3faa31ff9a73a3687bf5522b2035e561ac41')
