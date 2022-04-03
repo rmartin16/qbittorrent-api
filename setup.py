bd6c3913f0fb80ad6ebbf41ce3d27f6e24e6bdb5
rmartin16 <rmartin16@gmail.com>
2022-04-03 13:10:02 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="qbittorrent-api",
-    version="2022.3.29",
+    version="2022.4.30",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     install_requires=[
