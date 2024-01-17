f8ec6ff349ff1e1b31deabd9c88bddfb65507bbf
Russell Martin <russell@rjm.li>
2024-01-16 21:32:26 -0500

    Advertise support for qBittorrent v4.6.3

--- src/qbittorrentapi/_version_support.py
+++ src/qbittorrentapi/_version_support.py
@@ -56,9 +56,10 @@ APP_VERSION_2_API_VERSION_MAP: dict[str, str] = {
     "v4.6.0": "2.9.2",
     "v4.6.1": "2.9.3",
     "v4.6.2": "2.9.3",
+    "v4.6.3": "2.9.3",
 }
 
-MOST_RECENT_SUPPORTED_APP_VERSION: Final[Literal["v4.6.2"]] = "v4.6.2"
+MOST_RECENT_SUPPORTED_APP_VERSION: Final[Literal["v4.6.3"]] = "v4.6.3"
 MOST_RECENT_SUPPORTED_API_VERSION: Final[Literal["2.9.3"]] = "2.9.3"
 
 
