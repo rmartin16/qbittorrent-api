2eef204d160afbb14de48a6519b5f5e19c272c0e
Russell Martin <russell@rjm.li>
2024-10-19 16:16:39 -0400

    Add support to manage cookies - ``app/cookies`` - ``app/setCookies``

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import sys
 
 import pytest
 
-from qbittorrentapi import APINames
+from qbittorrentapi import APINames, CookieList
 from qbittorrentapi._attrdict import AttrDict
 from qbittorrentapi.app import (
     DirectoryContentList,
@@ -60,12 +60,76 @@ def test_preferences(client):
     assert dht is not client.app.preferences.dht
     client.app_set_preferences(prefs=dict(dht=dht))
     assert dht is client.app.preferences.dht
+    before_prefs = client.app.preferences
+    client.app.set_preferences(client.app.preferences)
+    assert before_prefs == client.app.preferences
 
 
 def test_default_save_path(client):
     assert "download" in client.app_default_save_path().lower()
     assert "download" in client.app_defaultSavePath().lower()
     assert "download" in client.app.default_save_path.lower()
+
+
+@pytest.mark.skipif_before_api_version("2.11.3")
+@pytest.mark.parametrize(
+    "set_cookies_func",
+    [
+        "app_set_cookies",
+        "app_setCookies",
+        "app.set_cookies",
+        "app.setCookies",
+    ],
+)
+def test_cookies(client, set_cookies_func):
+    client.func(set_cookies_func)()
+    assert client.app_cookies() == CookieList([])
+
+    cookie_one = {
+        "domain": "example.com",
+        "path": "/example/path",
+        "name": "cookie name",
+        "value": "cookie value",
+        "expirationDate": 1729366667,
+    }
+    cookie_two = {
+        "domain": "yahoo.jp",
+        "path": "/path",
+        "name": "name cookie",
+        "value": "value cookie",
+        "expirationDate": 1429366667,
+    }
+
+    client.app_set_cookies([cookie_one])
+    assert client.app.cookies == CookieList([cookie_one])
+
+    client.func(set_cookies_func)([cookie_two])
+    assert client.app.cookies == CookieList([cookie_two])
+
+    client.func(set_cookies_func)(CookieList([cookie_one, cookie_two]))
+    assert client.app.cookies == CookieList([cookie_one, cookie_two])
+
+    client.func(set_cookies_func)([])
+    assert client.app_cookies() == CookieList([])
+
+
+@pytest.mark.skipif_after_api_version("2.11.3")
+@pytest.mark.parametrize(
+    "set_cookies_func",
+    [
+        "app_set_cookies",
+        "app_setCookies",
+        "app.set_cookies",
+        "app.setCookies",
+    ],
+)
+def test_cookies_not_implemented(client, set_cookies_func):
+    with pytest.raises(NotImplementedError):
+        _ = client.app_cookies()
+    with pytest.raises(NotImplementedError):
+        _ = client.app.cookies
+    with pytest.raises(NotImplementedError):
+        client.func(set_cookies_func)([])
 
 
 @pytest.mark.skipif_before_api_version("2.3")
