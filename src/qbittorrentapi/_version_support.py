b0081e25a775f6c2710bfb8b6ccfbbda0691f2e8
Russell Martin <russell@rjm.li>
2024-12-21 11:55:10 -0500

    Advertise support for qBittorrent v5.0.3 and v5.1.0beta1

--- src/qbittorrentapi/_version_support.py
+++ src/qbittorrentapi/_version_support.py
@@ -66,9 +66,11 @@ APP_VERSION_2_API_VERSION_MAP: dict[str, str] = {
     "v5.0.0": "2.11.2",
     "v5.0.1": "2.11.2",
     "v5.0.2": "2.11.2",
+    "v5.1.0beta1": "2.11.3",
+    "v5.0.3": "2.11.2",
 }
 
-MOST_RECENT_SUPPORTED_APP_VERSION: Final[Literal["v5.0.2"]] = "v5.0.2"
+MOST_RECENT_SUPPORTED_APP_VERSION: Final[Literal["v5.0.3"]] = "v5.0.3"
 MOST_RECENT_SUPPORTED_API_VERSION: Final[Literal["2.11.2"]] = "2.11.2"
 
 
