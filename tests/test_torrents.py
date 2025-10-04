98f666dc199e24a1d15a06a10f3c5ce9c085e689
Russell Martin <russell@rjm.li>
2025-10-04 19:24:00 -0400

    add retry to test_rename_folder

--- tests/test_torrents.py
+++ tests/test_torrents.py
@@ -790,36 +790,40 @@ def test_rename_file_not_implemented(
     ],
 )
 def test_rename_folder(client, app_version, new_torrent, new_name, rename_folder_func):
-    if v(app_version) >= v("v4.3.3"):
-        # move the file in to a new folder
-        orig_file_path = new_torrent.files[0].name
-        new_folder = "qwer"
-        client.torrents_rename_file(
-            torrent_hash=new_torrent.hash,
-            old_path=orig_file_path,
-            new_path=new_folder + "/" + orig_file_path,
-        )
+    @retry()
+    def test():
+        if v(app_version) >= v("v4.3.3"):
+            # move the file in to a new folder
+            orig_file_path = new_torrent.files[0].name
+            new_folder = "qwer"
+            client.torrents_rename_file(
+                torrent_hash=new_torrent.hash,
+                old_path=orig_file_path,
+                new_path=new_folder + "/" + orig_file_path,
+            )
 
-        # wait for the folder to be renamed
-        check(
-            lambda: [f.name.split("/")[0] for f in new_torrent.files],
-            new_folder,
-            reverse=True,
-        )
+            # wait for the folder to be renamed
+            check(
+                lambda: [f.name.split("/")[0] for f in new_torrent.files],
+                new_folder,
+                reverse=True,
+            )
 
-        # test rename that new folder
-        client.func(rename_folder_func)(
-            torrent_hash=new_torrent.hash,
-            old_path=new_folder,
-            new_path=new_name,
-        )
-        check(
-            lambda: new_torrent.files[0].name.replace("+", " "),
-            new_name + "/" + orig_file_path,
-        )
-    elif v(app_version) >= v("v4.3.2"):
-        with pytest.raises(NotImplementedError):
-            client.func(rename_folder_func)()
+            # test rename that new folder
+            client.func(rename_folder_func)(
+                torrent_hash=new_torrent.hash,
+                old_path=new_folder,
+                new_path=new_name,
+            )
+            check(
+                lambda: new_torrent.files[0].name.replace("+", " "),
+                new_name + "/" + orig_file_path,
+            )
+        elif v(app_version) >= v("v4.3.2"):
+            with pytest.raises(NotImplementedError):
+                client.func(rename_folder_func)()
+
+    test()
 
 
 @pytest.mark.skipif_after_api_version("2.7")
