03e135812fa5bf16494e2a93348c20615c65ae64
Russell Martin <russell@rjm.li>
2026-05-09 13:03:23 -0400

    test for torrent interface too

--- tests/test_torrent.py
+++ tests/test_torrent.py
@@ -187,10 +187,15 @@ def test_set_share_limits(orig_torrent, set_share_limits_func):
         ratio_limit=5,
         seeding_time_limit=100,
         inactive_seeding_time_limit=200,
-        share_limit_action="STOP",
+        share_limit_action="Stop",
+        share_limits_mode="MatchAny",
     )
     check(lambda: orig_torrent.info.max_ratio, 5)
     check(lambda: orig_torrent.info.max_seeding_time, 100)
+    if "share_limit_action" in orig_torrent.info:
+        check(lambda: orig_torrent.info.share_limit_action, "Stop")
+    if "share_limits_mode" in orig_torrent.info:
+        check(lambda: orig_torrent.info.share_limits_mode, "MatchAny")
     if "max_inactive_seeding_time" in orig_torrent.info:
         check(lambda: orig_torrent.info.max_inactive_seeding_time, 200)
 
